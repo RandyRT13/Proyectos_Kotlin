92815396aaa46e0b2ad94ad3b1ceb75920be7090
RandyRT13 <reyesrandy1013@gmail.com>
2024-08-01 12:25:40 -0400

    Creando la funcion NotaItem, donde se verán las notas de la app

--- app/src/main/java/com/example/appnotas/MainActivity.kt
+++ app/src/main/java/com/example/appnotas/MainActivity.kt
@@ -8,7 +8,10 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
@@ -17,6 +20,7 @@ import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.appnotas.ui.theme.AppNotasTheme
@@ -53,15 +57,56 @@ fun AppNotas() {
             label = { Text("Titulo") },
             modifier = Modifier.fillMaxWidth()
         )
-        Spacer(modifier = )
+        Spacer(modifier = Modifier.height(8.dp))
+        TextField(
+            value = descripcion,
+            onValueChange = { descripcion = it },
+            label = { Text("Descripcion") },
+            modifier = Modifier.fillMaxWidth()
+        )
+        Spacer(modifier = Modifier.height(8.dp))
+        Button(
+            onClick = {
+                if (titulo.isNotEmpty() && descripcion.isNotEmpty()) {
+                    notas = notas + Nota(
+                        id = notas.size + 1,
+                        titulo = titulo,
+                        descripcion = descripcion
+                    )
+                }
+            
+            },
+            modifier = Modifier.align(Alignment.End)
+        ) {
+            Text("Nueva Nota")
+        }
+        Spacer(modifier = Modifier.height(16.dp))
+        LazyColumn {
+            items(notas) { nota ->
+                NotaItem(nota = nota, onDelete = {
+                    notas = notas.filter { it.id != note.id }
+                })
+            }
+        }
+
     }
 
+}
+
+@Composable
+fun NotaItem(nota: Nota, onDelete: () -> Unit) {
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(vertical = 4.dp),
+        elevation = 4.dp
+    ) {
+        Column(
+            modifier = Modifier.padding(16.dp)
+        ) {
+
+        }
+    }
 }
 
 @Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    AppNotasTheme {
-        Greeting("Android")
-    }
-}
