2c1f6aa61446ea512fdb57f4249294eff558bbd5
RandyRT13 <reyesrandy1013@gmail.com>
2024-08-01 14:23:45 -0400

    Base terminada (Al intentar guardar la nota se detiene la aplicacion :( )

--- app/src/main/java/com/example/appnotas/MainActivity.kt
+++ app/src/main/java/com/example/appnotas/MainActivity.kt
@@ -1,29 +1,39 @@
 package com.example.appnotas
 
+//noinspection UsingMaterialAndMaterial3Libraries
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ColumnScope
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Button
+import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
 import com.example.appnotas.ui.theme.AppNotasTheme
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -31,7 +41,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             AppNotasTheme {
-                Surface(color = MaterialTheme.colors.background) {
+                Surface(color = MaterialTheme.colorScheme.background) {
                     AppNotas()
                 }
 
@@ -84,7 +94,7 @@ fun AppNotas() {
         LazyColumn {
             items(notas) { nota ->
                 NotaItem(nota = nota, onDelete = {
-                    notas = notas.filter { it.id != note.id }
+                    notas = notas.filter { it.id != nota.id }
                 })
             }
         }
@@ -104,9 +114,40 @@ fun NotaItem(nota: Nota, onDelete: () -> Unit) {
         Column(
             modifier = Modifier.padding(16.dp)
         ) {
+            Text(text = nota.titulo, style = MaterialTheme.typography.headlineMedium)
+            Spacer(modifier = Modifier.height(4.dp))
+            Text(text = nota.descripcion)
+            Spacer(modifier = Modifier.height(8.dp))
+            Row (
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.End
+            ) {
+                Button(onClick = onDelete) {
+                    Text("Borrar")
+                    
+                }
+            }
+
 
         }
     }
 }
 
+fun Card(modifier: Modifier, elevation: Dp, content: @Composable() (ColumnScope.() -> Unit)) {
+    TODO("Not yet implemented")
+}
+
 @Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    AppNotasTheme {
+        AppNotas()
+    }
+}
+
+data class Nota(
+    val id: Int,
+    val titulo: String,
+    val descripcion: String
+)
+
